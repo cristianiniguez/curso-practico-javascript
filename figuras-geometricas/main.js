43067528a10b1fa1c06e462f03afd1b0a3f1fea4
cristianiniguez <felizpc1@gmail.com>
2022-04-21 21:28:31 -0400

    taller 1: figuras geometricas - calculando figuras geometricas en javascript

--- figuras-geometricas/main.js
+++ figuras-geometricas/main.js
@@ -1,1 +1,55 @@
-console.log('Hello World');
+// Square
+
+console.group('Square');
+
+const squareSide = 5;
+console.log(`Square sides are ${squareSide}cm`);
+
+const squarePerimeter = squareSide * 4;
+console.log(`Square perimeter is ${squarePerimeter}cm`);
+
+const squareArea = squareSide * squareSide;
+console.log(`Square area is ${squareArea}cm^2`);
+
+console.groupEnd('Square');
+
+// Triangle
+
+console.group('Triangle');
+
+const triangleSide1 = 6;
+const triangleSide2 = 6;
+const triangleBase = 4;
+console.log(`Triangle sides are ${triangleSide1}cm, ${triangleSide2}cm and ${triangleBase}cm`);
+
+const triangleHeight = 5.5;
+console.log(`Triangle height is ${triangleHeight}cm`);
+
+const trianglePerimeter = triangleSide1 + triangleSide2 + triangleBase;
+console.log(`Triangle perimeter is ${trianglePerimeter}cm`);
+
+const triangleArea = (triangleBase * triangleHeight) / 2;
+console.log(`Triangle area is ${triangleArea}cm^2`);
+
+console.groupEnd('Triangle');
+
+// Circle
+
+console.group('Circle');
+
+const circleRadius = 4;
+console.log(`Circle radius is ${circleRadius}cm`);
+
+const circleDiameter = circleRadius * 2;
+console.log(`Circle diameter is ${circleDiameter}cm`);
+
+const PI = Math.PI;
+console.log(`PI is ${PI}`);
+
+const circlePerimeter = PI * circleDiameter;
+console.log(`Circle perimeter is ${circlePerimeter}cm`);
+
+const circleArea = PI * circleRadius * circleRadius;
+console.log(`Circle area is ${circleArea}cm^2`);
+
+console.groupEnd('Circle');
