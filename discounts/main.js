12efda8f273a0b33278f30803468423b2a2dedc5
cristianiniguez <felizpc1@gmail.com>
2022-05-31 18:45:39 -0400

    taller 2: precios y descuentos - escribiendo html desde javascript

--- discounts/main.js
+++ discounts/main.js
@@ -10,3 +10,21 @@ function calculatePriceWithDiscount(price, discount) {
   const priceWithDiscount = (price * priceWithDiscountPercentage) / 100;
   return priceWithDiscount;
 }
+
+function onClickButtonPriceDiscount() {
+  const inputPrice = document.getElementById('input-price');
+  const inputDiscount = document.getElementById('input-discount');
+
+  if (!(inputPrice instanceof HTMLInputElement))
+    throw new Error('#input-price is not an HTMLInputElement');
+  if (!(inputDiscount instanceof HTMLInputElement))
+    throw new Error('#input-discount is not an HTMLInputElement');
+
+  const priceValue = parseFloat(inputPrice.value);
+  const discountValue = parseFloat(inputDiscount.value);
+
+  const priceWithDiscount = calculatePriceWithDiscount(priceValue, discountValue);
+
+  const result = document.getElementById('result');
+  result.innerText = 'The price with discount is: $' + priceWithDiscount;
+}
